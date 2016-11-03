cd32e1b7ee71735d8b17e9bc77b8b5746f273758
Artemij <gosha.lyulkin@yandex.ru>
2016-11-04 00:23:15 +0400

    Реализация самого простого списка

--- app/src/main/java/com/example/artemij/ppplus/MainActivity.java
+++ app/src/main/java/com/example/artemij/ppplus/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.artemij.ppplus;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textHeader;
+
+    String[] names = {"Артем", "Саня Ф.", "Саня К.", "Миша Ц.", "Аня", "Юля", "Миша М.", "Настя Б."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface myTypeFace = Typeface.createFromAsset(getAssets(), "HelveticaNeueCyr-Light.otf");
+        textHeader = (TextView)findViewById(R.id.textView1);
+        textHeader.setTypeface(myTypeFace);
+
+        ListView studList = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        studList.setAdapter(adapter);
     }
 }
